e96277106a14ab7714d08ef7d33ad63889bee629
Dev-Souza <kauan.souza.dev@gmail.com>
2025-03-18 20:32:53 -0300

    feat: add properties the style

--- react_native_IESB/App.js
+++ react_native_IESB/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
 
 export default function App() {
   const nome = "Dev-Kauan"
@@ -7,21 +7,42 @@ export default function App() {
     alert("Clicou no button")
   }
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Image 
-        source={{uri:"https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/09/GettyImages-1668971338-e1694439970587.jpg?w=1200&h=900&crop=1"}}
-        style={{
-          width: 200,
-          height: 200
-        }}  
-      />
-      
-      <Text>My project, Kauan S.</Text>
-      <Text>Bem vindo {nome}</Text>
-     
-      <Button title='Clicar' onPress={alerta}/>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Image
+          source={{ uri: "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/09/GettyImages-1668971338-e1694439970587.jpg?w=1200&h=900&crop=1" }}
+          style={styles.imageStyle}
+        />
+        <Image
+          source={require("./images/ney.png")}
+          style={styles.imageStyle}
+        />
+        <Image
+          source={require("./images/ney.png")}
+          style={styles.imageStyle}
+        />
+        <Image
+          source={require("./images/ney.png")}
+          style={styles.imageStyle}
+        />
+        <Image
+          source={require("./images/ney.png")}
+          style={styles.imageStyle}
+        />
+        <Image
+          source={require("./images/ney.png")}
+          style={styles.imageStyle}
+        />
+        <Image
+          source={require("./images/ney.png")}
+          style={styles.imageStyle}
+        />
+        <Text style={styles.text}>My project, Kauan S.</Text>
+        <Text>Bem vindo {nome}</Text>
+        <Button title='Clicar' onPress={alerta} />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -32,4 +53,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25,
+    fontWeight: 600
+  },
+  imageStyle: {
+    marginBottom: 30,
+    width: 200,
+    height: 200
+  }
 });
