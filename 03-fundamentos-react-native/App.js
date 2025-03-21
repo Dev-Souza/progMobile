af6e6835ed9cd133cd65f83c8e95d7ceeedb9961
Dev-Souza <kauan.souza.dev@gmail.com>
2025-03-21 20:18:37 -0300

    feat: Modify o App.js

--- 03-fundamentos-react-native/App.js
+++ 03-fundamentos-react-native/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FirstComponent from './components/FirstComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FirstComponent text="Olá" titulo="Dev Kauan"></FirstComponent>
     </View>
   );
 }
