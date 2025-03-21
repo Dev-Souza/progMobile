8b715d5a61efbcf56fbd2e5e7af1bb78b51f031e
Dev-Souza <kauan.souza.dev@gmail.com>
2025-03-21 20:28:35 -0300

    feat: Modify App.js for import the SecondComponent.jsx

--- 03-fundamentos-react-native/App.js
+++ 03-fundamentos-react-native/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import FirstComponent from './components/FirstComponent';
+import SecondComponent from './components/SecondComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <FirstComponent text="Olá" titulo="Dev Kauan"></FirstComponent>
+      <SecondComponent></SecondComponent>
     </View>
   );
 }
