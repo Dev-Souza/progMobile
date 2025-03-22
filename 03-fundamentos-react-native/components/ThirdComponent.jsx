eac368ae6be2af5f444e78c27fb6639b541cb54d
Dev-Souza <kauan.souza.dev@gmail.com>
2025-03-21 21:14:36 -0300

    feat

---
+++ 03-fundamentos-react-native/components/ThirdComponent.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ThirdComponent() {
+  return (
+    <View>
+      <Text>ThirdComponent</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
